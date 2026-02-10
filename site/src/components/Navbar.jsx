8c249f744ff38e58353781c19008bea24bae9a8f
Rifat Rahman Khan <Rifat@CS-JHONG8-2.local>
2026-02-09 22:16:07 -0500

    logo according to navbar size

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -1,59 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Logo from "../assets/iclab_logo.png"
 import { FaHome } from "react-icons/fa";
 import { IoDocumentTextSharp } from "react-icons/io5";
 import { FaPeopleGroup } from "react-icons/fa6";
 
-
 const Navbar = () => {
-     const [state, setState] = useState(false);
-     const [color, setColor] = useState('bg-amber-800');
-    const HandleClick = () =>{
-       setState(!state);
-    }
-    // const colorChange = () =>{
-    //     color == 'bg-amber-800' ? setColor("bg-green-500") : setColor("bg-amber-800");
-    //  }
-    // useEffect(() => {
-    //     console.log(color)
-    // },[color])
-    
+  const [state, setState] = useState(false);
+
   return (
     <>
-        <nav>
-            <div className="flex justify-between px-10 bg- py-5 text-white">
-                <div>
-                <img className='h-10 w-auto md:h-13 lg:h-14 object-contain' src={Logo} alt="Logo of ICLab@stevensLogo of the Interactive Computing Lab @ Stevens" />
-                
-                </div>
-                <div className='pr-[10]'>
-                <ul className="flex gap-4">
-                    <li><a href="#"><FaHome className='inline-block mx-1 mt-[-5px]'/>Home</a></li>
-                    <li><a href="#"><IoDocumentTextSharp className='inline-block mx-1 mt-[-5px]'/>Publications</a></li>
-                    <li><a href="#"><FaPeopleGroup className='inline-block mx-1 mt-[-5px]'/>Lab Members</a></li>
-                    
-                    
+      <nav className="bg-black text-white">
+        <div className="flex items-center justify-between px-10 h-16">
+          <div className="flex items-center">
+            <img
+              src={Logo}
+              alt="Logo of the Interactive Computing Lab at Stevens"
+              className="h-10 w-auto object-contain"
+            />
+          </div>
 
-                </ul>
-                </div>
-                {/* <div>
-                <button className="px-4 py-1.5 bg-blue-400 rounded-full">Click Here</button>
-                </div> */}
-            </div>
+          <ul className="flex items-center gap-6">
+            <li>
+              <a href="#" className="flex items-center gap-1 hover:text-gray-300">
+                <FaHome /> Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-1 hover:text-gray-300">
+                <IoDocumentTextSharp /> Publications
+              </a>
+            </li>
+            <li>
+              <a href="#" className="flex items-center gap-1 hover:text-gray-300">
+                <FaPeopleGroup /> Lab Members
+              </a>
+            </li>
+          </ul>
+        </div>
 
-            {
-                state?
-                <div className={`h-80 ${color}`}>
-                <h1>Hello</h1>
-                <button className="px-4 py-1.5 bg-blue-400 rounded-full" onClick={colorChange}>Click Here</button>
-                
-                </div>: null
-
-            }
-            
-        </nav>
-    
-    
+        {state && (
+          <div className="h-80 bg-amber-800"></div>
+        )}
+      </nav>
     </>
   )
 }
