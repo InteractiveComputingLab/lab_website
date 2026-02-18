dd8cc121e06aa5cdb98a8021cf997d8d80d6fd08
Rifat Rahman Khan <Rifat@CS-JHONG8-2.local>
2026-02-18 15:44:45 -0500

    navbar changes

--- site/src/components/Navbar.jsx
+++ site/src/components/Navbar.jsx
@@ -8,26 +8,16 @@ import { HiMenu, HiX } from "react-icons/hi";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setOpen((prev) => !prev);
-  };
-
-  const closeMenu = () => {
-    setOpen(false);
-  };
-
   return (
     <nav className="bg-black text-white">
-      <div className="px-6 lg:px-10 py-4 flex items-center justify-between">
-        
-        {/* Logo */}
+      <div className="px-6 lg:px-10 py-3 flex items-center justify-between">
+
         <img
           src={Logo}
           alt="Interactive Computing Lab logo"
-          className="h-[30px] sm:h-[30px] md:h-[35px] lg:h-[40px] xl:h-[40px] w-auto object-contain"
+          className="h-[35px] lg:h-[40px] w-auto object-contain"
         />
 
-        {/* Desktop Menu */}
         <ul className="hidden lg:flex items-center gap-6">
           <li>
             <a href="#" className="flex items-center gap-2 hover:text-gray-300">
@@ -46,45 +36,30 @@ const Navbar = () => {
           </li>
         </ul>
 
-        {/* Hamburger Button */}
         <button
-          onClick={toggleMenu}
+          onClick={() => setOpen(!open)}
           className="lg:hidden text-2xl"
           aria-label="Toggle navigation menu"
-          aria-expanded={open}
         >
           {open ? <HiX /> : <HiMenu />}
         </button>
       </div>
 
-      {/* Mobile Menu */}
       {open && (
         <div className="lg:hidden px-6 pb-4">
           <ul className="flex flex-col gap-4">
             <li>
-              <a
-                href="#"
-                onClick={closeMenu}
-                className="flex items-center gap-2 hover:text-gray-300"
-              >
+              <a href="#" className="flex items-center gap-2 hover:text-gray-300">
                 <FaHome /> Home
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                onClick={closeMenu}
-                className="flex items-center gap-2 hover:text-gray-300"
-              >
+              <a href="#" className="flex items-center gap-2 hover:text-gray-300">
                 <IoDocumentTextSharp /> Publications
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                onClick={closeMenu}
-                className="flex items-center gap-2 hover:text-gray-300"
-              >
+              <a href="#" className="flex items-center gap-2 hover:text-gray-300">
                 <FaPeopleGroup /> Lab Members
               </a>
             </li>
